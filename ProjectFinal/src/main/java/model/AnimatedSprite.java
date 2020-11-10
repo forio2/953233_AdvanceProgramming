af1ddbe64d3d0c7608aab2f9f94a12d01ae00c10
forio2 <forio2@hotmail.com>
2020-11-10 22:51:31 +0700

    Add final project

---
+++ ProjectFinal/src/main/java/model/AnimatedSprite.java
@@ -0,0 +1,63 @@
+package model;
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class AnimatedSprite extends ImageView{
+
+    int count, columns, offsetX, offsetY, width, height, curXIndex=0, curYIndex=0;
+
+    public AnimatedSprite(Image image, int count, int columns, int offsetX, int offsetY, int width, int height){
+        this.setImage(image);
+        this.count = count;
+        this.columns = columns;
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        this.width = width;
+        this.height = height;
+        this.setViewport(new Rectangle2D(offsetX, offsetY, width, height));
+    }
+
+    public void tick() {
+        curXIndex = (curXIndex+1)%columns;
+        curYIndex = (curYIndex+1)/columns;
+        System.out.println("first");
+        interpolate();
+    }
+
+    public void jump() {
+        curXIndex = (curXIndex+1)%columns;
+        curYIndex = (curYIndex+1)/columns;
+        System.out.println("second");
+        interpolate2();
+    }
+
+    public void dive() {
+        curXIndex = (curXIndex+1)%(columns-3);
+        curYIndex = (curYIndex+1)/(columns-3);
+        System.out.println("third");
+        interpolate3();
+    }
+
+    protected void interpolate() {
+        final int x = curXIndex*width+offsetX;
+        final int y = curYIndex*height+offsetY;
+        System.out.println("test: " + x + "and" + y );
+        this.setViewport(new Rectangle2D(x, y, width, height));
+    }
+
+    protected void interpolate2() {
+        final int x = curXIndex*width+offsetX;
+        final int y = curYIndex*height+offsetY+63;
+        System.out.println("test: " + x + "and" + y );
+        this.setViewport(new Rectangle2D(x, y, width, height));
+    }
+
+    protected void interpolate3() {
+        final int x = curXIndex*width+offsetX;
+        final int y = curYIndex*height+offsetY+127;
+        System.out.println("test: " + x + "and" + y );
+        this.setViewport(new Rectangle2D(x, y, width, height));
+    }
+}
---
+++ ProjectFinal/src/main/java/model/Character.java
@@ -0,0 +1,159 @@
+package model;
+
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import view.Platform;
+
+public class Character extends Pane {
+
+    Logger logger = LoggerFactory.getLogger(Character.class);
+
+    public static final int CHARACTER_WIDTH = 70;
+    public static final int CHARACTER_HEIGHT = 64;
+
+    private Image characterImg;
+
+
+    private AnimatedSprite imageView;
+
+    private int x;
+    private int y;
+
+    private KeyCode leftKey;
+    private KeyCode rightKey;
+    private KeyCode upKey;
+    private KeyCode ultiKey;
+
+    int xVelocity = 0;
+    int yVelocity = 0;
+    int xAcceleration = 1;
+    int yAcceleration = 1;
+    int xMaxVelocity = 7;
+    int yMaxVelocity = 20;
+    boolean isMoveLeft = false;
+    boolean isMoveRight = false;
+    boolean falling = true;
+    boolean canJump = false;
+    boolean jumping = false;
+
+    public Character(int x, int y, int offsetX, int offsetY, KeyCode leftKey, KeyCode rightKey, KeyCode upKey, KeyCode ultiKey) {
+
+        this.x = x;
+        this.y = y;
+        this.setTranslateX(x);
+        this.setTranslateY(y);
+        this.characterImg = new Image(getClass().getResourceAsStream("/assets/sprite_sheet.png"));
+        this.imageView = new AnimatedSprite(characterImg,4,7,offsetX,offsetY,66,64);
+        this.imageView.setFitWidth(CHARACTER_WIDTH);
+        this.imageView.setFitHeight(CHARACTER_HEIGHT);
+        this.leftKey = leftKey;
+        this.rightKey = rightKey;
+        this.upKey = upKey;
+        this.ultiKey = ultiKey;
+        this.getChildren().addAll(this.imageView);
+    }
+
+    public void moveLeft() {
+        isMoveLeft = true;
+        isMoveRight = false;
+    }
+    public void moveRight() {
+        isMoveRight = true;
+        isMoveLeft = false;
+    }
+
+    public void stop() {
+        isMoveLeft = false;
+        isMoveRight = false;
+        xVelocity = 0;
+    }
+
+    public void jump() {
+        if (canJump) {
+            yVelocity = yMaxVelocity;
+            canJump = false;
+            jumping = true;
+            falling = false;
+        }
+    }
+
+    public void checkReachHighest() {
+        if(jumping &&  yVelocity <= 0) {
+            jumping = false;
+            falling = true;
+            yVelocity = 0;
+        }
+    }
+
+    public void checkReachFloor() {
+        if(falling && y >= Platform.GROUND - CHARACTER_HEIGHT) {
+            falling = false;
+            canJump = true;
+            yVelocity = 0;
+        }
+    }
+
+    public void checkReachGameWall() {
+        if(x <= 0) {
+            x = 0;
+        } else if( x+getWidth() >= Platform.WIDTH) {
+            x = Platform.WIDTH-CHARACTER_WIDTH;
+        }
+    }
+
+    public void moveX() {
+        setTranslateX(x);
+
+        if(isMoveLeft) {
+            xVelocity = xVelocity >= xMaxVelocity? xMaxVelocity : xVelocity+xAcceleration;
+            x = x - xVelocity;
+        }
+        if(isMoveRight) {
+            xVelocity = xVelocity >= xMaxVelocity? xMaxVelocity : xVelocity+xAcceleration;
+            x = x + xVelocity;
+        }
+    }
+
+    public void moveY() {
+        setTranslateY(y);
+
+        if(falling) {
+            yVelocity = yVelocity >= yMaxVelocity? yMaxVelocity : yVelocity+yAcceleration;
+            y = y + yVelocity;
+        }
+        else if(jumping) {
+            yVelocity = yVelocity <= 0 ? 0 : yVelocity-yAcceleration;
+            y = y - yVelocity;
+        }
+    }
+
+    public void repaint() {
+        moveX();
+        moveY();
+    }
+
+    public void trace() {
+        logger.debug("x:{} y:{} vx:{} vy:{}",x,y,xVelocity,yVelocity);
+    }
+
+    public KeyCode getLeftKey() {
+        return leftKey;
+    }
+
+    public KeyCode getRightKey() {
+        return rightKey;
+    }
+
+    public KeyCode getUpKey() {
+        return upKey;
+    }
+
+    public KeyCode getUtilKey() {
+        return ultiKey;
+    }
+
+    public AnimatedSprite getImageView() { return imageView; }
+}
